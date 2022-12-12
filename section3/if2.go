8ec71a9d1a2bed5492a93f5da90833df66307ea2
su-pernova <sumikim323@naver.com>
2022-12-12 14:44:59 +0900

    ENH: IF, ELSE, ELSE IF 사용법 예제 학습

---
+++ section3/if2.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 제어문(조건문) : IF - ELSE 문을 사용할 수 있다.
+	var a int = 50
+	b := 70
+
+	if a >= 65 {
+		fmt.Println("a 는 65 이상이다.")
+	} else {
+		fmt.Println("a 는 65 미만이다.")
+	}
+
+	if b >= 70 {
+		fmt.Println("b 는 70 이상이다.")
+	} else {
+		fmt.Println("b 는 70 미만이다.")
+	}
+
+	/*
+		[ 에러가 발생하는 경우 ]
+		1. if 문 에서와 같이 else 문 중괄호 이전에 줄바꿈이 들어가면 에러가 발생한다.
+			if a >= 65 {
+				fmt.Println("a 는 65 이상이다.")
+			} else
+			{
+				fmt.Println("a 는 65 미만이다.")
+			}
+
+		2. else 문 선언 이전에 줄바꿈이 들어가도 에러가 발생한다.
+			if a >= 65 {
+				fmt.Println("a 는 65 이상이다.")
+			}
+			else {
+				fmt.Println("a 는 65 미만이다.")
+			}
+	*/
+}
---
+++ section3/if3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 제어문(조건문) : IF - ELSE IF - ELSE 문을 사용할 수 있다.
+	i := 40
+
+	if i >= 120 {
+		fmt.Println("i 는 120 이상이다.")
+	} else if i < 120 && i >= 100 {
+		fmt.Println("i 는 120 미만 100 이상이다.")
+	} else if i < 100 && i >= 50 {
+		fmt.Println("i 는 100 미만 50 이상이다.")
+	} else {
+		fmt.Println("i 는 50 미만이다.")
+	}
+}
